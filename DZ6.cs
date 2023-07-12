1676e3a91105f4d993488ff696ebbaf2cb89d504
Alena <magii.alena@yandex.ru>
2023-07-12 10:32:40 +0300

    end commit

--- DZ6.cs
+++ DZ6.cs
@@ -1,19 +1,19 @@
 ﻿// 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
-// Console.Write("Введите количество чисел в строке: ");
-// int sizeUser = Convert.ToInt32(Console.ReadLine());
-// int [] numbers = new int [sizeUser];
-// int count = 0;
-// for (int i = 0; i < sizeUser; i++)
-// {
-//     Console.Write($"Введите {i+1} число: ");
-//     numbers [i] = Convert.ToInt32(Console.ReadLine());
-//     if (numbers [i]>0)
-//     {
-//         count++;
-//     }
-// }
-// Console.WriteLine($"Количество положительных чисел {count}");
-// Console.WriteLine($"Числа: [ {string.Join("; ", numbers)} ]");
+Console.Write("Введите количество чисел в строке: ");
+int sizeUser = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [sizeUser];
+int count = 0;
+for (int i = 0; i < sizeUser; i++)
+{
+    Console.Write($"Введите {i+1} число: ");
+    numbers [i] = Convert.ToInt32(Console.ReadLine());
+    if (numbers [i]>0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество положительных чисел {count}");
+Console.WriteLine($"Числа: [ {string.Join("; ", numbers)} ]");
 
 // 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
 Console.Write("Введите число b1: ");
